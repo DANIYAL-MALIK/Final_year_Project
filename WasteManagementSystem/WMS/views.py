84ffbfa1636f42c1970c884813dfe38b795cfe6b
Daniyal Malik <myEmail@example.com>
2022-01-15 16:39:30 +0500

    Working on complaint form

--- WasteManagementSystem/WMS/views.py
+++ WasteManagementSystem/WMS/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html',)
+def about(request):
+    return render(request,'about.html')
+def Complaint(request):
+    return render(request,'complaint.html', {})
     
-
+    
 # Create your views here.
